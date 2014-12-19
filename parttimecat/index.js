29fa451eebc8932f3890a40546357a14ef4d2214
weeksun23 <jy013018@aliyun.com>
2014-12-19 16:27:38 +0800

    更新cat

--- parttimecat/index.js
+++ parttimecat/index.js
@@ -1,30 +1,28 @@
-(function(){
-	new IScroll("#indexList");
-	Mobilebone.callback = function(pageInto, pageOut) {
-		if(pageInto == pageOut) return;
-		var inIsWindow = pageInto.getAttribute("data-window") !== null;
-		var nav = document.querySelector("body>.nav");
+new IScroll("#indexList");
+Mobilebone.callback = function(pageInto, pageOut) {
+	if(pageInto == pageOut) return;
+	var inIsWindow = pageInto.getAttribute("data-window") !== null;
+	var nav = document.querySelector("body>.nav");
+	if(pageOut){
+		var outIsWindow = pageOut.getAttribute("data-window") !== null;
+		if(!outIsWindow || !inIsWindow){
+			if(outIsWindow && !inIsWindow){
+				nav.className = "footer nav slide reverse in";
+			}else if(!outIsWindow && inIsWindow){
+				nav.className = "footer nav slide out";
+			}
+		}
+	}
+	if(!inIsWindow){
+		var linkIn = nav.querySelector("a[href$="+ pageInto.id +"]");
+		linkIn.classList.add("active");
 		if(pageOut){
-			var outIsWindow = pageOut.getAttribute("data-window") !== null;
-			if(!outIsWindow || !inIsWindow){
-				if(outIsWindow && !inIsWindow){
-					nav.className = "footer nav slide reverse in";
-				}else if(!outIsWindow && inIsWindow){
-					nav.className = "footer nav slide out";
-				}
+			if(!outIsWindow){
+				var linkOut = nav.querySelector("a[href$="+ pageOut.id +"]");
+				linkOut.classList.remove("active");
 			}
+		}else{
+			nav.className = "footer nav in";
 		}
-		if(!inIsWindow){
-			var linkIn = nav.querySelector("a[href$="+ pageInto.id +"]");
-			linkIn.classList.add("active");
-			if(pageOut){
-				if(!outIsWindow){
-					var linkOut = nav.querySelector("a[href$="+ pageOut.id +"]");
-					linkOut.classList.remove("active");
-				}
-			}else{
-				nav.className = "footer nav in";
-			}
-		}
-	};
-})();
+	}
+};
