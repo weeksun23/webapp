49232e053904a414b999a3a712d92f46eaca36e2
linyz <jy013018@aliyun.com>
2015-04-18 08:44:24 +0800

    重命名slidedelete

--- slideitem/slideitem.js
+++ slideitem/slideitem.js
@@ -1,4 +1,7 @@
-define(function(){
+/*
+依赖base avalon
+*/
+define(['avalon'],function(avalon){
     'use strict';
 	var transformName = 'transform' in document.documentElement.style ? 'transform' : 'webkitTransform';
     var numReg = /\-?[0-9]+\.?[0-9]*/g;
@@ -13,20 +16,26 @@ define(function(){
     function dealTranslateEnd(el,x){
         if(x === 0){
             el.querySelector(".list-item-del").classList.add('hide');
-            if(this.curDelEl === el){
-                this.curDelEl = null;
+            if(this.curSlideItem === el){
+                this.curSlideItem = null;
             }
         }else{
-            this.curDelEl = el;
+            this.curSlideItem = el;
         }
     }
 	/*
+    实例后提供滑动item所需的start move end事件函数
+    目前只支持向左滑
 	*/
-	function SlideDelete(options){
-		this.options = options || {};
-		this.curDelEl = null;
+	function SlideItem(options){
+        this.options = avalon.mix({
+            //滑动后要显示的元素的选择器
+            targetSelector : ''
+        },options);
+        //当前滑动的元素
+		this.curSlideItem = null;
 	}
-    SlideDelete.prototype.setTranslateXAnimate = function(el,x,start,func){
+    SlideItem.prototype.setTranslateXAnimate = function(el,x,start,func){
         var me = this;
         if(start === undefined){
             start = getTranslateX(el);
@@ -61,28 +70,28 @@ define(function(){
             }
         });
     };
-	SlideDelete.prototype.getEventObj = function(Base,el){
+	SlideItem.prototype.getEventObj = function(Base,el){
 		var me = this;
 		var data = {};
 		return {
 			_start : function(e){
                 if(el.$animating) return;
-                if(me.curDelEl && me.curDelEl !== el){
-                    me.setTranslateXAnimate(me.curDelEl,0);
+                if(me.curSlideItem && me.curSlideItem !== el){
+                    me.setTranslateXAnimate(me.curSlideItem,0);
                 }
             	data.touchstart = Base.getTouchPos(e);
             	data.posstart = getTranslateX(el);
-                var del = el.querySelector(".list-item-del");
+                var del = el.querySelector(me.options.targetSelector);
             	del.classList.remove('hide');
-                data.delWidth = avalon(del.querySelector("div")).width();
+                data.targetWidth = avalon(del.querySelector("div")).width();
             },
             _move : function(e){
                 if(el.$animating) return;
             	var touch = Base.getTouchPos(e);
             	var moved = touch.x - data.touchstart.x;
             	var curPos = data.posstart + moved;
-            	if(curPos < -data.delWidth){
-            		curPos = -data.delWidth;
+            	if(curPos < -data.targetWidth){
+            		curPos = -data.targetWidth;
             	}else if(curPos > 0){
             		curPos = 0;
             	}
@@ -91,8 +100,8 @@ define(function(){
             _end : function(){
                 if(el.$animating) return;
                 var x = getTranslateX(el);
-                if(Math.abs(x) > data.delWidth / 2){
-                    var target = -data.delWidth;
+                if(Math.abs(x) > data.targetWidth / 2){
+                    var target = -data.targetWidth;
                 }else{
                     target = 0;
                 }
@@ -100,5 +109,5 @@ define(function(){
             }
 		};
 	};
-	return SlideDelete;
+	return SlideItem;
 });
